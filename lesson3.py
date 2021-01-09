727f67d36524e54bc25fd495984ee6a9d0fc45c4
Ilgiz <test@gmail.com>
2021-01-09 15:56:28 +0500

    Создали модель человека : eat, work, act

---
+++ lesson3.py
@@ -0,0 +1,64 @@
+# git init - создание git репозитория
+# git config user.name 'Ilgiz'
+# git config user.email 'test@gmail.com'
+
+
+# Реализуем модель человека.
+# Человек может есть, работать, играть, ходить в магазин.
+# У человека есть степень сытости, немного еды и денег.
+# Если сытость < 0 единиц, человек умирает.
+# Человеку надо прожить 365 дней.
+
+
+from termcolor import cprint
+import random
+
+class Human:
+
+    def __init__(self, name):
+        self.name = name 
+        self.fullness = 50 
+        self.house = None 
+
+    def __str__(self):
+        return f'Я - {self.name}, сытость {self.fullness}'
+
+
+    def work(self):
+        cprint(f'{self.name} сходил на работу', color='blue')
+        self.money += 50
+        self.fullness -= 10
+
+
+    def eat(self):
+        if self.house.food >= 10:
+            cprint(f'{self.name} - поел', color='yellow')
+            self.fullness += 10
+            self.house.food -= 10
+        else:
+            cprint(f'{self.name} - нет еды', color='red')
+
+
+    def act(self):
+        if self.fullness <= 0:
+            cprint(f'{self.name} - умер ...', color='red')
+            return
+        dice = random.randint(1, 6)
+        if dice == 1:
+            self.work()
+        elif dice == 2:
+            pass
+        
+
+
+
+class House:
+    pass
+
+
+    
+
+
+# Создадим двух людей, живущих в одном доме - Бивиса и Батхеда
+# Нужен класс Дом, в нем должен быть холодильник с едой и тумбочка с деньгами
+# Еда пусть хранится в холодильнике в доме, а деньги - в тумбочке.
