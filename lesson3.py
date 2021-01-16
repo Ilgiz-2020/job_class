318128ade23d72bca9d2551e3284c9e788a21133
Vladislav <test@test.com>
2021-01-16 15:36:22 +0500

    Добавили новые классы : smm, gamer

--- lesson3.py
+++ lesson3.py
@@ -123,8 +123,53 @@ class Programmer(Human):
                 self.play_game()
 
 
+class Gamer(Human):
+    def train(self):
+        cprint(f'{self.name} - тренируется', color='green')
+        self.fullness -= 10
 
+    def championship(self):
+        cprint(f'{self.name} - участвует в чемпионате по Dota2', color='blue')
+        self.house.money += 50
+        self.fullness -= 20
 
+    def learn(self):
+        cprint(f'{self.name} - изучает теорию игр', color='green')
+        self.fullness -= 10
+
+    def act(self):
+        super().act()
+        if self.action == False and self.fullness >= 10:
+            if self.dice == 4:
+                self.train()
+            elif self.dice == 5:
+                self.championship()
+            elif self.dice == 6:
+                self.learn()
+
+class SMM(Human):
+    def write_post(self):
+        cprint(f'{self.name} - пишет пост для Facebook', color='green')
+        self.fullness -= 10
+
+    def picture(self):
+        cprint(f'{self.name} - готовит картинку для поста на Facebook', color='blue')
+        self.house.money += 50
+        self.fullness -= 20
+
+    def stories(self):
+        cprint(f'{self.name} - снимает сторис для Instagram', color='green')
+        self.fullness -= 10
+
+    def act(self):
+        super().act()
+        if self.action == False and self.fullness >= 10:
+            if self.dice == 4:
+                self.write_post()
+            elif self.dice == 5:
+                self.picture()
+            elif self.dice == 6:
+                self.stories()
 
 
 
